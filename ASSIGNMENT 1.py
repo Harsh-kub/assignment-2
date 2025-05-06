b456cddc8ba32a22f5c579d1aeb2844654622143
Harsh-kub <harshkubade12345@gmail.com>
2025-05-06 14:15:26 +0530

    Create ASSIGNMENT 1.py

---
+++ ASSIGNMENT 1.py
@@ -0,0 +1,17 @@
+# ASSIGNMENT 2
+# TASK 1:
+
+
+n=int(input("Enter a number: "))
+if n%2==0:
+    print(n,"is a Even Number")
+else:
+    print(n ," is a Odd Number")
+
+# TASK 2:
+
+n=50
+for i in range(1,n):
+    n=i+n
+
+print(" the sum of numbers from 1 to 50 is ", n)
